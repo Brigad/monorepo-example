c9bf6323de76a01c36301f8bb9e21c695600db9b
Thibault Malbranche <thibault.malbranche@epitech.eu>
2020-01-13 16:31:48 +0100

    wip

--- packages/mobileapp/babel.config.js
+++ packages/mobileapp/babel.config.js
@@ -1,3 +1,38 @@
+const path = require('path');
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          react: require.resolve('react', {
+            paths: [path.join(__dirname, './')],
+          }),
+          '^react-native$': require.resolve('react-native', {
+            paths: [path.join(__dirname, './')],
+          }),
+          '^react-native/(.+)': ([, name]) =>
+            require.resolve(`react-native/${name}`, {
+              paths: [path.join(__dirname, './')],
+            }),
+        },
+        extensions: [
+          '.ios.js',
+          '.ios.ts',
+          '.ios.tsx',
+          '.android.js',
+          '.android.ts',
+          '.android.tsx',
+          '.native.js',
+          '.native.ts',
+          '.native.tsx',
+          '.js',
+          '.ts',
+          '.tsx',
+        ],
+      },
+    ],
+  ],
 };
